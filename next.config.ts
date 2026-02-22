a143e0e531db01c4e6ab3e36c018db0cddfb6c56
Sachin <sachin@geekonomy.in>
2026-02-22 13:21:10 +0530

    Enhance image handling by adding remote image patterns in next.config.ts, include new dependencies for react-virtuoso and swagger UI types in package.json, and update gallery items to use remote images in constants.ts.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "pub-8ef2b42cbdf94e97a856dc5176f846d8.r2.dev",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
